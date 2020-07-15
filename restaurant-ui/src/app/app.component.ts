d3e5bb16ab0663331b1d23c4c5d18ded1c996305
Nils Hoffmann <the.nilshoffmann@gmail.com>
2020-07-15 22:22:38 +0200

    Initial configuration for strapi with example call in Angular

--- restaurant-ui/src/app/app.component.ts
+++ restaurant-ui/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
+  selector:    'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:   ['./app.component.css']
 })
 export class AppComponent {
   title = 'restaurant-ui';
+  public data: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  public callStrapi(): void {
+    this.http.get('http://localhost:1337/restaurants').subscribe(data => this.data = data);
+  }
 }
--- restaurant-ui/src/app/app.module.ts
+++ restaurant-ui/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
